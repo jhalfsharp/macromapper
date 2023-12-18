9e986fc1b49c56c3999558e7bbf6b6feb16421ba
Jimmy Mack <jimmy@bluewiz.net>
2023-12-17 23:13:52 -0800

    Boilerplate and basic app outline

---
+++ src/main.rs
@@ -0,0 +1,289 @@
+use egui::{emath::RectTransform, Visuals};
+use egui_extras::{Size, StripBuilder};
+use ico::*;
+use macroquad::prelude::*;
+use undo::History;
+
+mod drawing;
+use drawing::*;
+mod utils;
+use utils::*;
+mod tools;
+use tools::*;
+
+//Import images
+const DRAG_SVG: &[u8; 832] = include_bytes!("../assets/icons/d_drag.svg");
+const RECT_SVG: &[u8; 495] = include_bytes!("../assets/icons/d_rect.svg");
+
+//Window setup
+fn default_conf() -> Conf {
+    let file = std::io::Cursor::new(include_bytes!("../assets/logo/macromapper.ico"));
+    //i use unwrap here like 7 times and i guess technically its bad practice
+    //but the icon isnt gonna change so it shouldnt panic if it launches once
+    //anyway yeah sue me
+    let dir = IconDir::read(file).unwrap();
+    Conf {
+        window_title: "Macromapper".to_string(),
+        window_width: 1200,
+        window_height: 675,
+        high_dpi: true,
+        icon: Some(miniquad::conf::Icon {
+            big: dir.entries()[0]
+                .decode()
+                .unwrap()
+                .rgba_data()
+                .try_into()
+                .unwrap(),
+            medium: dir.entries()[1]
+                .decode()
+                .unwrap()
+                .rgba_data()
+                .try_into()
+                .unwrap(),
+            small: dir.entries()[2]
+                .decode()
+                .unwrap()
+                .rgba_data()
+                .try_into()
+                .unwrap(),
+        }),
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(default_conf)]
+async fn main() {
+    //Mouse globals
+    let mut mouse_old = vec2(0., 0.);
+    let mut mouse_new: Vec2;
+    let mut is_dragging: bool = false;
+    let mut mouse_pressed_old: bool = false;
+    let mut mouse_pressed_new: bool;
+    let mut mouse_in_egui: bool = false;
+    let mut mouse_grid: Vec2;
+    let mut mouse_grid_snapped: Vec2;
+    let mut drag_started: Vec2 = vec2(0., 0.);
+    let mut snap: f32 = 0.5;
+
+    //Camera globals
+    let res_scale: f32; //screen DPI scale (usually 1.0)
+    unsafe {
+        //I really wish I didn't have to use any unsafe, but apparently the DPI scale is dangerous
+        res_scale = get_internal_gl().quad_context.dpi_scale();
+    }
+    let mut camera = Cam::new(res_scale);
+
+    //Image loading and rasterizing
+    //again, images shouldnt change so unwrap is safe if it launches once
+    let drag_img = egui_extras::RetainedImage::from_svg_bytes("drag", DRAG_SVG).unwrap();
+    let rect_img = egui_extras::RetainedImage::from_svg_bytes("rect", RECT_SVG).unwrap();
+
+    //App state globals
+    let mut tool: Box<dyn Tool> = Box::new(DragTool {});
+    let mut selected_tool: i8 = 1;
+    let mut tool_type = true;
+
+    let mut layers: Vec<Layer> = vec![Layer::new()];
+    let mut layer_histories: Vec<History<_>> = vec![History::<PolyOp>::new()];
+    let mut active_layer: usize = 0;
+
+    loop {
+        egui_macroquad::ui(|egui_ctx| {
+            egui::TopBottomPanel::top("top_panel").show(egui_ctx, |ui| {
+                // The top panel is often a good place for a menu bar:
+                egui::menu::bar(ui, |ui| {
+                    ui.menu_button("File", |ui| {
+                        if ui.button("New").clicked() {
+                            ui.close_menu();
+                        }
+                        ui.menu_button("Preferences", |ui| {
+                            if ui.button("Dark mode").clicked() {
+                                egui_ctx.set_visuals(Visuals::dark());
+                            }
+                            if ui.button("Light mode").clicked() {
+                                egui_ctx.set_visuals(Visuals::light());
+                            }
+                        });
+                    });
+                    ui.menu_button("View", |ui| {
+                        if ui.button("Go to origin").clicked() {
+                            camera.update_focus(vec2(0., 0.));
+                            ui.close_menu();
+                        }
+                    });
+                });
+            });
+            egui::TopBottomPanel::bottom("bottom_panel").show(egui_ctx, |ui| {
+                ui.label("Test");
+            });
+            egui::SidePanel::left("left_panel").show(egui_ctx, |ui| {
+                StripBuilder::new(ui)
+                    .size(Size::exact(35.0))
+                    .size(Size::remainder())
+                    .horizontal(|mut strip| {
+                        strip.cell(|ui| {
+                            if ui
+                                .add(
+                                    egui::ImageButton::new(
+                                        drag_img.texture_id(egui_ctx),
+                                        drag_img.size_vec2(),
+                                    )
+                                    .selected(selected_tool == 1),
+                                )
+                                .clicked()
+                            {
+                                tool = Box::new(DragTool {});
+                                selected_tool = 1;
+                            }
+                            if ui
+                                .add(
+                                    egui::ImageButton::new(
+                                        rect_img.texture_id(egui_ctx),
+                                        rect_img.size_vec2(),
+                                    )
+                                    .selected(selected_tool == 2),
+                                )
+                                .clicked()
+                            {
+                                tool = Box::new(RectTool::new());
+                                selected_tool = 2;
+                            }
+                        });
+                        strip.cell(|ui| {
+                            ui.label("Test");
+                            ui.horizontal(|ui| {
+                                ui.add(toggle(&mut tool_type)).labelled_by(
+                                    ui.label(if tool_type { "Draw" } else { "Erase" }).id,
+                                );
+                            });
+                            egui::ComboBox::from_label("Snap fraction")
+                                .selected_text(snap.to_frac_string())
+                                .show_ui(ui, |ui| {
+                                    ui.selectable_value(&mut snap, 1.0, "1");
+                                    ui.selectable_value(&mut snap, 0.5, "1/2");
+                                    ui.selectable_value(&mut snap, 1.0 / 3.0, "1/3");
+                                    ui.selectable_value(&mut snap, 0.25, "1/4");
+                                    ui.selectable_value(&mut snap, 0.2, "1/5");
+                                    ui.selectable_value(&mut snap, 1.0 / 6.0, "1/6");
+                                });
+                            ui.add(
+                                egui::Slider::new(&mut camera.scale, 10.0..=200.0)
+                                    .text("Grid spacing"),
+                            );
+                            //Fill with empty space to allow resizing
+                            //ui.allocate_space(ui.available_size());
+                        })
+                    });
+            });
+            egui::SidePanel::right("right_panel").show(egui_ctx, |ui| {
+                ui.label("Test");
+            });
+            let available = egui_ctx.available_rect();
+            let screen_to_relative = RectTransform::from_to(
+                egui_ctx.used_rect(),
+                egui::Rect::from_x_y_ranges(0.0..=screen_width(), 0.0..=screen_height()),
+            );
+            let canvas = screen_to_relative.transform_rect(available);
+            //Update main rect
+            if camera.screen_rect.w != canvas.width() || camera.screen_rect.h != canvas.height() {
+                camera.screen_rect.x = canvas.left();
+                camera.screen_rect.y = screen_height() - canvas.bottom();
+                camera.screen_rect.w = canvas.width();
+                camera.screen_rect.h = canvas.height();
+                camera.update_focus(camera.focus);
+            }
+            mouse_in_egui = egui_ctx.is_using_pointer();
+        });
+
+        // Process keys, mouse etc.
+        mouse_new = mouse_position().into();
+        mouse_pressed_new = is_mouse_button_down(MouseButton::Left);
+
+        //This could be shorter but that sacrifices clarity
+        if mouse_pressed_new && !mouse_in_egui {
+            if !mouse_pressed_old && camera.screen_rect.contains(mouse_new) {
+                drag_started = mouse_new.clone();
+                is_dragging = true;
+            }
+        } else {
+            is_dragging = false;
+        }
+
+        // Handle coordinates
+        mouse_grid = (mouse_new - camera.screen_rect.point()) * vec2(1.0, -1.0)
+            + camera.grid_rect.point()
+            + vec2(0.0, camera.grid_rect.h);
+        mouse_grid_snapped = (mouse_grid / camera.scale / snap).round() * camera.scale * snap;
+
+        //Update based on input
+        if is_dragging {
+            tool.drag(mouse_new, mouse_old, &mut camera);
+        }
+
+        if !mouse_pressed_new
+            && mouse_pressed_old
+            && camera.screen_rect.contains(mouse_new)
+            && !mouse_in_egui
+            && drag_started == mouse_old
+        {
+            if let Some(i) = tool.left_click(
+                mouse_grid_snapped,
+                if tool_type {
+                    &PolyOpType::Union
+                } else {
+                    &PolyOpType::Subtraction
+                },
+            ) {
+                (layer_histories[active_layer]).edit(&mut layers[active_layer], i);
+            }
+        }
+
+        mouse_old = mouse_new;
+        mouse_pressed_old = mouse_pressed_new;
+
+        //Set up camera
+        set_camera(&camera.to_camera());
+
+        //Grid
+        clear_background(WHITE);
+        let mut grid = Sketch::new(1., LIGHTGRAY);
+        ((camera.grid_rect.left() / camera.scale) as i32
+            ..=(camera.grid_rect.right() / camera.scale) as i32)
+            .map(|i| {
+                grid.add(Line::new(
+                    i as f32 * camera.scale,
+                    camera.grid_rect.top(),
+                    i as f32 * camera.scale,
+                    camera.grid_rect.bottom(),
+                ));
+            })
+            .count();
+        ((camera.grid_rect.top() / camera.scale) as i32
+            ..=(camera.grid_rect.bottom() / camera.scale) as i32)
+            .map(|i| {
+                grid.add(Line::new(
+                    camera.grid_rect.left(),
+                    i as f32 * camera.scale,
+                    camera.grid_rect.right(),
+                    i as f32 * camera.scale,
+                ));
+            })
+            .count();
+        grid.draw();
+        for l in layers.iter().rev() {
+            l.draw();
+        }
+
+        //draw_circle(0., 0., 20., YELLOW);
+
+        //Draw snapped cursor circle
+        draw_circle(mouse_grid_snapped.x, mouse_grid_snapped.y, 3.0, RED);
+
+        tool.preview(mouse_grid_snapped, 1., RED).draw();
+
+        egui_macroquad::draw();
+        // Draw things after egui
+
+        next_frame().await;
+    }
+}
---
+++ src/tools.rs
@@ -0,0 +1,73 @@
+use crate::{drawing::*, utils::*};
+use macroquad::prelude::*;
+
+pub trait Tool {
+    fn left_click(&mut self, pos: Vec2, op_type: &PolyOpType) -> Option<PolyOp>;
+    fn right_click(&mut self, pos: Vec2) -> Option<PolyOp>;
+    fn drag(&mut self, mouse_new: Vec2, mouse_old: Vec2, camera: &mut Cam) -> Option<PolyOp>;
+    fn preview(&mut self, pos: Vec2, thickness: f32, color: Color) -> Sketch;
+}
+
+#[derive(PartialEq)]
+pub struct DragTool {}
+
+impl Tool for DragTool {
+    fn left_click(&mut self, _pos: Vec2, _op_type: &PolyOpType) -> Option<PolyOp> {
+        None
+    }
+    fn right_click(&mut self, _pos: Vec2) -> Option<PolyOp> {
+        None
+    }
+    fn drag(&mut self, mouse_new: Vec2, mouse_old: Vec2, camera: &mut Cam) -> Option<PolyOp> {
+        camera.update_focus(camera.focus + (mouse_new - mouse_old) * (Vec2::NEG_X + Vec2::Y));
+        None
+    }
+    fn preview(&mut self, _pos: Vec2, thickness: f32, color: Color) -> Sketch {
+        Sketch::new(thickness, color)
+    }
+}
+
+#[derive(PartialEq)]
+pub struct RectTool {
+    point: Option<Vec2>, // first part of rectangle (none when not in use)
+}
+
+impl RectTool {
+    pub fn new() -> Self {
+        RectTool { point: None }
+    }
+}
+
+impl Tool for RectTool {
+    fn left_click(&mut self, pos: Vec2, op_type: &PolyOpType) -> Option<PolyOp> {
+        match self.point {
+            Some(_) => {
+                let out = poly_rect(self.point.expect("there should be a first point"), pos);
+                self.point = None;
+                Some(PolyOp::new(op_type.clone(), out))
+            }
+            None => {
+                self.point = Some(pos);
+                None
+            }
+        }
+    }
+    fn right_click(&mut self, _pos: Vec2) -> Option<PolyOp> {
+        self.point = None;
+        None
+    }
+    fn drag(&mut self, _mouse_new: Vec2, _mouse_old: Vec2, _camera: &mut Cam) -> Option<PolyOp> {
+        None
+    }
+    fn preview(&mut self, pos: Vec2, thickness: f32, color: Color) -> Sketch {
+        let mut out = Sketch::new(thickness, color);
+        if self.point.is_some() {
+            let point = self.point.unwrap();
+            out.add(Line::new(point.x, point.y, pos.x, point.y));
+            out.add(Line::new(point.x, pos.y, pos.x, pos.y));
+            out.add(Line::new(point.x, point.y, point.x, pos.y));
+            out.add(Line::new(pos.x, point.y, pos.x, pos.y));
+        }
+        out
+    }
+}
